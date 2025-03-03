b0496564b4058b124b948a3b46dea7b59d54bc8b
zahidblackduck <zahidul@blackduck.com>
2025-03-03 16:03:48 +0600

    cargo dependency transformer add

--- detectable/src/main/java/com/blackduck/integration/detectable/detectables/cargo/CargoDependencyTransformer.java
+++ detectable/src/main/java/com/blackduck/integration/detectable/detectables/cargo/CargoDependencyTransformer.java
@@ -12,29 +12,84 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 //TODO: Proposed CargoDependency Parser, Need to be implemented to parser
+
 public class CargoDependencyTransformer {
+
     public DependencyGraph transform(JsonObject jsonObject) {
         BasicDependencyGraph graph = new BasicDependencyGraph();
-        Map<String, Dependency> dependencies = new HashMap<>();
+        Map<String, Dependency> packageIdToDependency = new HashMap<>();
+        Set<String> allDependencies = new HashSet<>(); // Track all dependencies
 
+        // First pass: Create all Dependency objects
         JsonArray packages = jsonObject.getAsJsonArray("packages");
-        if (packages != null) {
-            for (JsonElement packageElement : packages) {
-                JsonObject packageObj = packageElement.getAsJsonObject();
-                String name = packageObj.get("name").getAsString();
-                String version = packageObj.get("version").getAsString();
-                String uniqueKey = name + ":" + version;
+        for (JsonElement pkg : packages) {
+            JsonObject packageObj = pkg.getAsJsonObject();
+            String packageId = packageObj.get("id").getAsString();
+            String name = packageObj.get("name").getAsString();
+            String version = packageObj.get("version").getAsString();
 
-                ExternalId externalId = new ExternalId(Forge.CRATES);
-                Dependency dependency = new ProjectDependency(name, version, externalId);
-                dependencies.put(uniqueKey, dependency);
+            ExternalId externalId = new ExternalId(Forge.CRATES);
+            externalId.setName(name);
+            externalId.setVersion(version);
+
+            Dependency dep = new ProjectDependency(name, version, externalId);
+            packageIdToDependency.put(packageId, dep);
+        }
+
+        // Second pass: Build dependency relationships
+        for (JsonElement pkg : packages) {
+            JsonObject packageObj = pkg.getAsJsonObject();
+            String currentPackageId = packageObj.get("id").getAsString();
+            Dependency currentDep = packageIdToDependency.get(currentPackageId);
+
+            JsonArray deps = packageObj.getAsJsonArray("dependencies");
+            for (JsonElement dep : deps) {
+                JsonObject depObj = dep.getAsJsonObject();
+                String depName = depObj.get("name").getAsString();
+                String depReq = depObj.get("req").getAsString(); // Version requirement
+
+                String depPackageId = findPackageId(packages, depName, depReq);
+                if (depPackageId != null) {
+                    Dependency childDep = packageIdToDependency.get(depPackageId);
+                    graph.addParentWithChild(currentDep, childDep);
+                    allDependencies.add(depPackageId); // Mark as a dependency
+                }
+            }
+        }
+
+        // Third pass: Identify root dependencies (not in allDependencies set)
+        for (JsonElement pkg : packages) {
+            JsonObject packageObj = pkg.getAsJsonObject();
+            String packageId = packageObj.get("id").getAsString();
+            Dependency dependency = packageIdToDependency.get(packageId);
+
+            if (!allDependencies.contains(packageId)) {
                 graph.addDirectDependency(dependency);
             }
         }
 
         return graph;
+    }
+
+    private String findPackageId(JsonArray packages, String name, String req) {
+        for (JsonElement pkg : packages) {
+            JsonObject packageObj = pkg.getAsJsonObject();
+            String packageName = packageObj.get("name").getAsString();
+            String packageVersion = packageObj.get("version").getAsString();
+
+            if (packageName.equals(name) && satisfiesVersionRequirement(packageVersion, req)) {
+                return packageObj.get("id").getAsString();
+            }
+        }
+        return null;
+    }
+
+    private boolean satisfiesVersionRequirement(String version, String req) {
+        return version.startsWith(req.replace("^", "").replace("~", ""));
     }
 }
