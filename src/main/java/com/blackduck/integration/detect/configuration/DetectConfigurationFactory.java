dee1a2035e0568261d81d774bacd0341f1861239
zahidblackduck <zahidul@blackduck.com>
2025-03-08 02:53:12 +0600

    detect.cargo.path property fix to allow setting cargo build tool path

--- src/main/java/com/blackduck/integration/detect/configuration/DetectConfigurationFactory.java
+++ src/main/java/com/blackduck/integration/detect/configuration/DetectConfigurationFactory.java
@@ -476,7 +476,6 @@ public class DetectConfigurationFactory {
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_BAZEL_PATH),
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_CONAN_PATH),
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_CONDA_PATH),
-            detectConfiguration.getPathOrNull(DetectProperties.DETECT_CARGO_PATH),
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_CPAN_PATH),
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_CPANM_PATH),
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_DART_PATH),
@@ -496,7 +495,8 @@ public class DetectConfigurationFactory {
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_SWIFT_PATH),
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_SBT_PATH),
             detectConfiguration.getPathOrNull(DetectProperties.DETECT_LERNA_PATH),
-            detectConfiguration.getPathOrNull(DetectProperties.DETECT_OPAM_PATH)
+            detectConfiguration.getPathOrNull(DetectProperties.DETECT_OPAM_PATH),
+            detectConfiguration.getPathOrNull(DetectProperties.DETECT_CARGO_PATH)
         );
     }
 
