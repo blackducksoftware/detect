e45274558f8dd8b95ae09b564f0436141b27e026
shantyk <shanty@blackduck.com>
2025-04-11 05:51:28 -0600

    mend

--- src/main/java/com/blackduck/integration/detect/configuration/DetectProperties.java
+++ src/main/java/com/blackduck/integration/detect/configuration/DetectProperties.java
@@ -1945,6 +1945,8 @@ public class DetectProperties {
             }
         }
         DETECT_TOOLS.addDeprecatedValueInfo(DetectTool.THREAT_INTEL.name(), "reason");
+        DETECT_TOOLS_EXCLUDED.addDeprecatedValueInfo(DetectTool.THREAT_INTEL.name(), "reason");
+
         return new Properties(properties);
     }
 
