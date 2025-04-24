6cc0c5e92af656bb6f8631d27a98c69b4a1a1b6b
devmehtabd <devmehta@blackduck.com>
2025-04-24 09:41:10 -0400

    Update bd-common version and update project version model

--- src/main/java/com/blackduck/integration/detect/workflow/blackduck/project/SyncProjectOperation.java
+++ src/main/java/com/blackduck/integration/detect/workflow/blackduck/project/SyncProjectOperation.java
@@ -60,6 +60,7 @@ public class SyncProjectOperation {
 
         Optional.ofNullable(projectSyncOptions.getProjectVersionPhase()).ifPresent(projectSyncModel::setPhase);
         Optional.ofNullable(projectSyncOptions.getProjectVersionDistribution()).ifPresent(projectSyncModel::setDistribution);
+        Optional.ofNullable(projectSyncOptions.isForceProjectVersionUpdate()).ifPresent(projectSyncModel::setUpdate);
 
         Integer projectTier = projectSyncOptions.getProjectTier();
         if (null != projectTier && projectTier >= 1 && projectTier <= 5) {
