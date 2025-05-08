7f35d400ad7cb31cdd5615b4f51aea0d5628b1f4
Andrian Sevastyanov <andrians@blackduck.com>
2025-05-08 11:03:44 -0600

    Address feedback regarding log messages

--- src/main/java/com/blackduck/integration/detect/SpringConfigErrorListener.java
+++ src/main/java/com/blackduck/integration/detect/SpringConfigErrorListener.java
@@ -15,8 +15,8 @@ public class SpringConfigErrorListener implements ApplicationListener<Applicatio
     public void onApplicationEvent(ApplicationFailedEvent event) {
         logger.error("An exception of type {} was encountered during application configuration.", event.getException().getClass());
         logger.error("The exception message was: {}", event.getException().getMessage());
-        logger.error("Please check https://documentation.blackduck.com/bundle/detect/page/configuring/overview.html for possible configuration sources and values.");            
         logger.error("A likely cause is an unparseable configuration file.");
+        logger.error("Please check https://documentation.blackduck.com/bundle/detect/page/configuring/overview.html for possible configuration sources and values.");
 
         // issue an exit call to prevent a long confusing stacktrace from being printed by Spring
         System.exit(ExitCodeType.FAILURE_CONFIGURATION.getExitCode());
