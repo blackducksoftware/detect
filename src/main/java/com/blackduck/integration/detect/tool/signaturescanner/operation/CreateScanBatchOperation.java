f02efcc4be44db4a705d64ebb79e56cc413b2135
dterrybd <dterry@blackduck.com>
2025-06-24 13:32:32 -0400

    update scan cli required version

--- src/main/java/com/blackduck/integration/detect/tool/signaturescanner/operation/CreateScanBatchOperation.java
+++ src/main/java/com/blackduck/integration/detect/tool/signaturescanner/operation/CreateScanBatchOperation.java
@@ -25,8 +25,7 @@ import com.blackduck.integration.util.NameVersion;
 
 public class CreateScanBatchOperation {
     private static final SignatureScannerVersion MIN_CSV_ARCHIVE_VERSION = new SignatureScannerVersion(2025, 1, 0);
-    // TODO need to update to 1.0.0 before release
-    private static final SignatureScannerVersion MIN_SCASS_VERSION = new SignatureScannerVersion(0, 1, 4);
+    private static final SignatureScannerVersion MIN_SCASS_VERSION = new SignatureScannerVersion(1, 0, 0);
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final BlackDuckSignatureScannerOptions signatureScannerOptions;
     private final DirectoryManager directoryManager;
