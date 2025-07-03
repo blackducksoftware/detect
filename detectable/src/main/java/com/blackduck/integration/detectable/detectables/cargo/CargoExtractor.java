924309215fbe84f12f9e184d0957b1dc4d2eb7d6
zahidblackduck <zahidul@blackduck.com>
2025-07-03 15:55:31 +0600

    remove wildcard imports

--- detectable/src/main/java/com/blackduck/integration/detectable/detectables/cargo/CargoExtractor.java
+++ detectable/src/main/java/com/blackduck/integration/detectable/detectables/cargo/CargoExtractor.java
@@ -3,7 +3,15 @@ package com.blackduck.integration.detectable.detectables.cargo;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.blackduck.integration.detectable.detectable.util.EnumListFilter;
