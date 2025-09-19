4ca9da483e549c5ea524e2daed3be1418fc8d206
Andrian Sevastyanov <andrians@blackduck.com>
2025-09-19 12:38:22 -0600

    Fix test syntax

--- src/test/java/com/blackduck/integration/detect/tool/detector/DetectorRuleFactoryTest.java
+++ src/test/java/com/blackduck/integration/detect/tool/detector/DetectorRuleFactoryTest.java
@@ -161,6 +161,7 @@ class DetectorRuleFactoryTest {
                             detectorNames.add(detectorName);
                         }
                     }
+                }
             }
         }
 
