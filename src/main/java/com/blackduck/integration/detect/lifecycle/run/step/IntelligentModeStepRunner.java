fcf348857239673746c6419f743b62d4a824f498
devmehtabd <devmehta@blackduck.com>
2025-10-10 14:43:45 -0400

    Fix logging for diaganostic zip

--- src/main/java/com/blackduck/integration/detect/lifecycle/run/step/IntelligentModeStepRunner.java
+++ src/main/java/com/blackduck/integration/detect/lifecycle/run/step/IntelligentModeStepRunner.java
@@ -8,7 +8,12 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.Queue;
 import java.util.UUID;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.List;
 import java.util.ArrayList;
@@ -379,7 +384,7 @@ public class IntelligentModeStepRunner {
         Optional<UUID> scanId = containerScanStepRunner.invokeContainerScanningWorkflow();
         scanId.ifPresent(uuid -> {
             scanIdsToWaitFor.add(uuid.toString());
-            logger.debug("Waiting for comtainer scan id {}", scanId);
+            logger.debug("Waiting for container scan id {}", scanId);
             scanWaiter.startWaitingForScan(uuid.toString(), blackDuckRunData, projectVersion, "Container Scan");
         });
         Set<String> containerScanCodeLocations = new HashSet<>();
