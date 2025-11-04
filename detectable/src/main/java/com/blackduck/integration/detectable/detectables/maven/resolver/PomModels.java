da14783ab30c4506f0ffacfc5cf84e0bf3bf32a5
Samrat Mukherjee <samratmuk@blackduck.com>
2025-11-04 14:34:56 +0530

    poc: add additional files

---
+++ detectable/src/main/java/com/blackduck/integration/detectable/detectables/maven/resolver/PomModels.java
@@ -0,0 +1,311 @@
+package com.blackduck.integration.detectable.detectables.maven.resolver;
+
+/**
+ * Data model classes for Maven POM parsing - Java port of Go javamodels
+ */
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import java.util.List;
+import java.util.Map;
+
+// Main POM XML model
+@JacksonXmlRootElement(localName = "project")
+class PomXml {
+    @JacksonXmlProperty(localName = "groupId")
+    private String groupId;
+
+    @JacksonXmlProperty(localName = "artifactId")
+    private String artifactId;
+
+    @JacksonXmlProperty(localName = "version")
+    private String version;
+
+    @JacksonXmlProperty(localName = "packaging")
+    private String packaging;
+
+    @JacksonXmlProperty(localName = "groupId", namespace = "parent")
+    private String parentGroupId;
+
+    @JacksonXmlProperty(localName = "artifactId", namespace = "parent")
+    private String parentArtifactId;
+
+    @JacksonXmlProperty(localName = "version", namespace = "parent")
+    private String parentVersion;
+
+    @JacksonXmlElementWrapper(localName = "parent")
+    @JacksonXmlProperty(localName = "relativePath")
+    private List<String> parentRelativePath;
+
+    @JacksonXmlElementWrapper(localName = "repositories")
+    @JacksonXmlProperty(localName = "repository")
+    private List<MavenRepositoryXml> repositories;
+
+    @JacksonXmlElementWrapper(localName = "dependencies")
+    @JacksonXmlProperty(localName = "dependency")
+    private List<PomXmlDependency> dependencies;
+
+    @JacksonXmlElementWrapper(localName = "dependencyManagement")
+    @JacksonXmlProperty(localName = "dependencies")
+    private List<PomXmlDependency> dependencyManagement;
+
+    @JacksonXmlElementWrapper(localName = "modules")
+    @JacksonXmlProperty(localName = "module")
+    private List<String> modules;
+
+    @JacksonXmlElementWrapper(localName = "build")
+    @JacksonXmlProperty(localName = "plugins")
+    private List<String> plugins;
+
+    // Getters and Setters
+    public String getGroupId() { return groupId; }
+    public void setGroupId(String groupId) { this.groupId = groupId; }
+
+    public String getArtifactId() { return artifactId; }
+    public void setArtifactId(String artifactId) { this.artifactId = artifactId; }
+
+    public String getVersion() { return version; }
+    public void setVersion(String version) { this.version = version; }
+
+    public String getPackaging() { return packaging; }
+    public void setPackaging(String packaging) { this.packaging = packaging; }
+
+    public String getParentGroupId() { return parentGroupId; }
+    public void setParentGroupId(String parentGroupId) { this.parentGroupId = parentGroupId; }
+
+    public String getParentArtifactId() { return parentArtifactId; }
+    public void setParentArtifactId(String parentArtifactId) { this.parentArtifactId = parentArtifactId; }
+
+    public String getParentVersion() { return parentVersion; }
+    public void setParentVersion(String parentVersion) { this.parentVersion = parentVersion; }
+
+    public List<String> getParentRelativePath() { return parentRelativePath; }
+    public void setParentRelativePath(List<String> parentRelativePath) { this.parentRelativePath = parentRelativePath; }
+
+    public List<MavenRepositoryXml> getRepositories() { return repositories; }
+    public void setRepositories(List<MavenRepositoryXml> repositories) { this.repositories = repositories; }
+
+    public List<PomXmlDependency> getDependencies() { return dependencies; }
+    public void setDependencies(List<PomXmlDependency> dependencies) { this.dependencies = dependencies; }
+
+    public List<PomXmlDependency> getDependencyManagement() { return dependencyManagement; }
+    public void setDependencyManagement(List<PomXmlDependency> dependencyManagement) { this.dependencyManagement = dependencyManagement; }
+
+    public List<String> getModules() { return modules; }
+    public void setModules(List<String> modules) { this.modules = modules; }
+
+    public List<String> getPlugins() { return plugins; }
+    public void setPlugins(List<String> plugins) { this.plugins = plugins; }
+}
+
+// Maven Repository XML model
+class MavenRepositoryXml {
+    @JacksonXmlProperty(localName = "id")
+    private String id;
+
+    @JacksonXmlProperty(localName = "name")
+    private String name;
+
+    @JacksonXmlProperty(localName = "url")
+    private String url;
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+}
+
+// POM XML Dependency Exclusion
+class PomXmlDependencyExclusion {
+    @JacksonXmlProperty(localName = "groupId")
+    private String groupId;
+
+    @JacksonXmlProperty(localName = "artifactId")
+    private String artifactId;
+
+    public String getGroupId() { return groupId; }
+    public void setGroupId(String groupId) { this.groupId = groupId; }
+
+    public String getArtifactId() { return artifactId; }
+    public void setArtifactId(String artifactId) { this.artifactId = artifactId; }
+}
+
+// POM XML Dependency
+class PomXmlDependency {
+    @JacksonXmlProperty(localName = "groupId")
+    private String groupId;
+
+    @JacksonXmlProperty(localName = "artifactId")
+    private String artifactId;
+
+    @JacksonXmlProperty(localName = "version")
+    private String version;
+
+    @JacksonXmlProperty(localName = "scope")
+    private String scope;
+
+    @JacksonXmlProperty(localName = "classifier")
+    private String classifier;
+
+    @JacksonXmlProperty(localName = "type")
+    private String type;
+
+    @JacksonXmlProperty(localName = "systemPath")
+    private String filePath;
+
+    @JacksonXmlProperty(localName = "optional")
+    private String optional;
+
+    @JacksonXmlElementWrapper(localName = "exclusions")
+    @JacksonXmlProperty(localName = "exclusion")
+    private List<PomXmlDependencyExclusion> exclusions;
+
+    // Getters and Setters
+    public String getGroupId() { return groupId; }
+    public void setGroupId(String groupId) { this.groupId = groupId; }
+
+    public String getArtifactId() { return artifactId; }
+    public void setArtifactId(String artifactId) { this.artifactId = artifactId; }
+
+    public String getVersion() { return version; }
+    public void setVersion(String version) { this.version = version; }
+
+    public String getScope() { return scope; }
+    public void setScope(String scope) { this.scope = scope; }
+
+    public String getClassifier() { return classifier; }
+    public void setClassifier(String classifier) { this.classifier = classifier; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getFilePath() { return filePath; }
+    public void setFilePath(String filePath) { this.filePath = filePath; }
+
+    public String getOptional() { return optional; }
+    public void setOptional(String optional) { this.optional = optional; }
+
+    public List<PomXmlDependencyExclusion> getExclusions() { return exclusions; }
+    public void setExclusions(List<PomXmlDependencyExclusion> exclusions) { this.exclusions = exclusions; }
+}
+
+// POM XML Coordinates (simplified model for coordinate-only parsing)
+class PomXmlCoordinates {
+    @JacksonXmlProperty(localName = "groupId", namespace = "parent")
+    private String parentGroupId;
+
+    @JacksonXmlProperty(localName = "artifactId", namespace = "parent")
+    private String parentArtifactId;
+
+    @JacksonXmlProperty(localName = "version", namespace = "parent")
+    private String parentVersion;
+
+    @JacksonXmlProperty(localName = "groupId")
+    private String groupId;
+
+    @JacksonXmlProperty(localName = "artifactId")
+    private String artifactId;
+
+    @JacksonXmlProperty(localName = "version")
+    private String version;
+
+    // Getters and Setters
+    public String getParentGroupId() { return parentGroupId; }
+    public void setParentGroupId(String parentGroupId) { this.parentGroupId = parentGroupId; }
+
+    public String getParentArtifactId() { return parentArtifactId; }
+    public void setParentArtifactId(String parentArtifactId) { this.parentArtifactId = parentArtifactId; }
+
+    public String getParentVersion() { return parentVersion; }
+    public void setParentVersion(String parentVersion) { this.parentVersion = parentVersion; }
+
+    public String getGroupId() { return groupId; }
+    public void setGroupId(String groupId) { this.groupId = groupId; }
+
+    public String getArtifactId() { return artifactId; }
+    public void setArtifactId(String artifactId) { this.artifactId = artifactId; }
+
+    public String getVersion() { return version; }
+    public void setVersion(String version) { this.version = version; }
+}
+
+// Java Coordinates
+class JavaCoordinates {
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String type;
+
+    public JavaCoordinates() {}
+
+    public JavaCoordinates(String groupId, String artifactId, String version, String type) {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.version = version;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public String getGroupId() { return groupId; }
+    public void setGroupId(String groupId) { this.groupId = groupId; }
+
+    public String getArtifactId() { return artifactId; }
+    public void setArtifactId(String artifactId) { this.artifactId = artifactId; }
+
+    public String getVersion() { return version; }
+    public void setVersion(String version) { this.version = version; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+}
+
+// Java Repository
+class JavaRepository {
+    private String id;
+    private String name;
+    private String url;
+
+    // Getters and Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+}
+
+// Java Dependency
+class JavaDependency {
+    private List<String> includedBy;
+    private JavaCoordinates coordinates;
+    private String scope;
+    private String type;
+    private String classifier;
+    private String projectDependencyFilePath;
+
+    // Getters and Setters
+    public List<String> getIncludedBy() { return includedBy; }
+    public void setIncludedBy(List<String> includedBy) { this.includedBy = includedBy; }
+
+    public JavaCoordinates getCoordinates() { return coordinates; }
+    public void setCoordinates(JavaCoordinates coordinates) { this.coordinates = coordinates; }
+
+    public String getScope() { return scope; }
+    public void setScope(String scope) { this.scope = scope; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getClassifier() { return classifier; }
+    public void setClassifier(String classifier) { this.classifier = classifier; }
+
+    public String getProjectDependencyFilePath() { return projectDependencyFilePath; }
+    public void setProjectDependencyFilePath(String projectDependencyFilePath) { this.projectDependencyFilePath = projectDependencyFilePath; }
+}
---
+++ detectable/src/main/java/com/blackduck/integration/detectable/detectables/maven/resolver/PropertiesResolver.java
@@ -0,0 +1,182 @@
+package com.blackduck.integration.detectable.detectables.maven.resolver;
+
+/**
+ * Property resolution classes - Java port of Go properties package
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Resolves properties for maven models.
+ */
+class PropertyResolver {
+    private Map<String, String> resolvedProperties;
+    private Map<String, String> properties;
+    private Map<String, String> parentProperties;
+    private Function<String, String> envPropertiesProvider;
+
+    public PropertyResolver(Map<String, String> resolvedProperties,
+                            Map<String, String> properties,
+                            Map<String, String> parentProperties,
+                            Function<String, String> envPropertiesProvider) {
+        this.resolvedProperties = resolvedProperties != null ? new HashMap<>(resolvedProperties) : new HashMap<>();
+        this.properties = properties != null ? new HashMap<>(properties) : new HashMap<>();
+        this.parentProperties = parentProperties != null ? new HashMap<>(parentProperties) : new HashMap<>();
+        this.envPropertiesProvider = envPropertiesProvider;
+    }
+
+    /**
+     * Resolves a property by name following Maven's property resolution order.
+     *
+     * @param propertyName The name of the property to resolve
+     * @return The resolved property value, or null if not found
+     */
+    public String resolve(String propertyName) {
+        // Check resolved properties first (highest priority)
+        if (resolvedProperties.containsKey(propertyName)) {
+            return resolvedProperties.get(propertyName);
+        }
+
+        // Check project properties
+        if (properties.containsKey(propertyName)) {
+            return properties.get(propertyName);
+        }
+
+        // Check parent properties
+        if (parentProperties.containsKey(propertyName)) {
+            return parentProperties.get(propertyName);
+        }
+
+        // Check environment variables (lowest priority)
+        if (envPropertiesProvider != null) {
+            return envPropertiesProvider.apply(propertyName);
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns all properties combined for use with StringSubstitutor.
+     */
+    public Map<String, String> getAllProperties() {
+        Map<String, String> allProps = new HashMap<>();
+
+        // Add in reverse priority order (lower priority first)
+        if (parentProperties != null) {
+            allProps.putAll(parentProperties);
+        }
+        if (properties != null) {
+            allProps.putAll(properties);
+        }
+        if (resolvedProperties != null) {
+            allProps.putAll(resolvedProperties);
+        }
+
+        return allProps;
+    }
+
+    // Getters
+    public Map<String, String> getResolvedProperties() { return resolvedProperties; }
+    public Map<String, String> getProperties() { return properties; }
+    public Map<String, String> getParentProperties() { return parentProperties; }
+}
+
+/**
+ * Enables the creation of PropertyResolver objects.
+ */
+class PropertiesResolverProvider {
+    private Map<String, String> userJvmProps;
+    private Map<String, String> parentProperties;
+    private Map<String, String> jvmProps;
+    private Function<String, String> envPropertiesProvider;
+
+    /**
+     * Creates a new PropertiesResolverProvider.
+     *
+     * @param userJvmProps User defined properties via the jvm "-D" option
+     * @param envPropertiesProvider Function that provides properties set by the environment
+     */
+    public PropertiesResolverProvider(Map<String, String> userJvmProps,
+                                      Function<String, String> envPropertiesProvider) {
+        this.userJvmProps = userJvmProps != null ? new HashMap<>(userJvmProps) : new HashMap<>();
+        this.jvmProps = getJavaCalcProperties();
+        this.envPropertiesProvider = envPropertiesProvider;
+        this.parentProperties = new HashMap<>();
+    }
+
+    /**
+     * Sets the parent properties for property resolution.
+     */
+    public void setParentProperties(Map<String, String> parentProps) {
+        this.parentProperties = parentProps != null ? new HashMap<>(parentProps) : new HashMap<>();
+    }
+
+    /**
+     * Creates a new PropertyResolver with the given properties.
+     *
+     * @param pomProps Properties sourced from the pom.xml file
+     * @param projectStarProperties Well-known properties that don't need resolution
+     * @return A new PropertyResolver instance
+     */
+    public PropertyResolver newResolver(Map<String, String> pomProps, Map<String, String> projectStarProperties) {
+        // Clone to avoid mutations
+        Map<String, String> clonedPomProps = pomProps != null ? new HashMap<>(pomProps) : new HashMap<>();
+
+        Map<String, String> resolvedProps = new HashMap<>();
+
+        // Add properties in priority order (lowest to highest)
+
+        // 1. Java system properties (lowest priority for collisions with pom props)
+        for (Map.Entry<String, String> entry : jvmProps.entrySet()) {
+            if (!clonedPomProps.containsKey(entry.getKey())) {
+                resolvedProps.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        // 2. Parent properties (skip on pom props collision)
+        for (Map.Entry<String, String> entry : parentProperties.entrySet()) {
+            if (!clonedPomProps.containsKey(entry.getKey())) {
+                resolvedProps.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        // 3. User JVM properties (higher priority)
+        resolvedProps.putAll(userJvmProps);
+
+        // 4. Project star properties (highest priority)
+        if (projectStarProperties != null) {
+            resolvedProps.putAll(projectStarProperties);
+        }
+
+        return new PropertyResolver(resolvedProps, clonedPomProps, parentProperties, envPropertiesProvider);
+    }
+
+    /**
+     * Gets calculated Java system properties.
+     * This is a simplified version - in a full implementation, this would include
+     * properties like ${java.version}, ${os.name}, etc.
+     */
+    private Map<String, String> getJavaCalcProperties() {
+        Map<String, String> props = new HashMap<>();
+
+        // Add some basic Java system properties
+        props.put("java.version", System.getProperty("java.version", ""));
+        props.put("java.home", System.getProperty("java.home", ""));
+        props.put("os.name", System.getProperty("os.name", ""));
+        props.put("os.arch", System.getProperty("os.arch", ""));
+        props.put("user.name", System.getProperty("user.name", ""));
+        props.put("user.home", System.getProperty("user.home", ""));
+        props.put("file.separator", System.getProperty("file.separator", "/"));
+
+        // Maven-specific calculated properties would be added here
+        // like ${maven.build.timestamp}, ${project.basedir}, etc.
+
+        return props;
+    }
+
+    // Getters
+    public Map<String, String> getParentProperties() { return parentProperties; }
+    public Map<String, String> getUserJvmProps() { return userJvmProps; }
+}
