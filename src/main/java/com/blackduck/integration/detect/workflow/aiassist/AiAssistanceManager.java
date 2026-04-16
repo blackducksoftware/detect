021a4458be236eb0cb87a1ba059e3b4103a6c12d
Samrat Mukherjee <samratmuk@blackduck.com>
2026-04-16 14:48:00 +0530

    formatting

--- src/main/java/com/blackduck/integration/detect/workflow/aiassist/AiAssistanceManager.java
+++ src/main/java/com/blackduck/integration/detect/workflow/aiassist/AiAssistanceManager.java
@@ -182,7 +182,6 @@ public class AiAssistanceManager {
         writer.println(SEPARATOR);
         writer.println("AI-Suggested Detect Configuration:");
         writer.println();
-        writer.println("  ./detect.sh \\");
 
         String[] keys = flags.keySet().toArray(new String[0]);
         for (int i = 0; i < keys.length; i++) {
