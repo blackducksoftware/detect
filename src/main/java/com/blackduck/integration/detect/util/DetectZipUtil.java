37bfe4e5384003549cd77b2564606f2068e36189
bd-spratikbharti <spratikbharti@blackduck.com>
2026-06-02 17:32:04 +0530

    Removed the unixPermission

--- src/main/java/com/blackduck/integration/detect/util/DetectZipUtil.java
+++ src/main/java/com/blackduck/integration/detect/util/DetectZipUtil.java
@@ -8,17 +8,13 @@ import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.EnumSet;
 import java.util.Enumeration;
 import java.util.Map;
-import java.util.Set;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipFile;
+import org.apache.commons.compress.utils.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,10 +54,10 @@ public class DetectZipUtil { //TODO: Add method for extracting without the wrapp
 
     public static void unzip(File zip, File dest, Charset charset) throws IOException {
         Path destPath = dest.toPath();
-        try (ZipFile zipFile = ZipFile.builder().setFile(zip).setCharset(charset).get()) {
-            Enumeration<ZipArchiveEntry> entries = zipFile.getEntries();
+        try (ZipFile zipFile = new ZipFile(zip, ZipFile.OPEN_READ, charset)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
             while (entries.hasMoreElements()) {
-                ZipArchiveEntry entry = entries.nextElement();
+                ZipEntry entry = entries.nextElement();
                 Path entryPath = destPath.resolve(entry.getName());
                 if (!entryPath.normalize().startsWith(dest.toPath().normalize())) {
                     throw new IOException("Zip entry contained path traversal");
@@ -71,37 +67,12 @@ public class DetectZipUtil { //TODO: Add method for extracting without the wrapp
                 } else {
                     Files.createDirectories(entryPath.getParent());
                     try (InputStream in = zipFile.getInputStream(entry)) {
-                        Files.copy(in, entryPath, StandardCopyOption.REPLACE_EXISTING);
+                        try (OutputStream out = new FileOutputStream(entryPath.toFile())) {
+                            IOUtils.copy(in, out);
+                        }
                     }
-                    applyUnixPermissions(entryPath, entry.getUnixMode());
                 }
             }
-        }
-    }
-
-    private static void applyUnixPermissions(Path filePath, int unixMode) {
-        if (unixMode == 0) {
-            return; // no Unix permissions stored in this entry
-        }
-        try {
-            Set<PosixFilePermission> permissions = EnumSet.noneOf(PosixFilePermission.class);
-            if ((unixMode & 0400) != 0) permissions.add(PosixFilePermission.OWNER_READ);
-            if ((unixMode & 0200) != 0) permissions.add(PosixFilePermission.OWNER_WRITE);
-            if ((unixMode & 0100) != 0) permissions.add(PosixFilePermission.OWNER_EXECUTE);
-            if ((unixMode & 0040) != 0) permissions.add(PosixFilePermission.GROUP_READ);
-            if ((unixMode & 0020) != 0) permissions.add(PosixFilePermission.GROUP_WRITE);
-            if ((unixMode & 0010) != 0) permissions.add(PosixFilePermission.GROUP_EXECUTE);
-            if ((unixMode & 0004) != 0) permissions.add(PosixFilePermission.OTHERS_READ);
-            if ((unixMode & 0002) != 0) permissions.add(PosixFilePermission.OTHERS_WRITE);
-            if ((unixMode & 0001) != 0) permissions.add(PosixFilePermission.OTHERS_EXECUTE);
-            Files.setPosixFilePermissions(filePath, permissions);
-        } catch (UnsupportedOperationException e) {
-            // Windows filesystems do not support POSIX permissions; fall back to setExecutable
-            if ((unixMode & 0111) != 0) {
-                filePath.toFile().setExecutable(true, false);
-            }
-        } catch (IOException e) {
-            logger.warn("Could not set permissions on extracted file: {}", filePath);
         }
     }
 }
