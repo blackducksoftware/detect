6eb108c8d981172aa367986fe932556bbd2905ec
zahidblackduck <zahidul@blackduck.com>
2026-07-27 10:47:51 +0600

    remove unused imports

--- src/test/java/com/blackduck/integration/detect/battery/docker/SetuptoolsTest.java
+++ src/test/java/com/blackduck/integration/detect/battery/docker/SetuptoolsTest.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
