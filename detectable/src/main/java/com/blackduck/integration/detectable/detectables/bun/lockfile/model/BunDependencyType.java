bc6d82e37f5c05acddb7ee78289b187819f616dc
zahidblackduck <zahidul@blackduck.com>
2026-09-18 20:21:36 +0600

    add bun package dependency and enum

---
+++ detectable/src/main/java/com/blackduck/integration/detectable/detectables/bun/lockfile/model/BunDependencyType.java
@@ -0,0 +1,9 @@
+package com.blackduck.integration.detectable.detectables.bun.lockfile.model;
+
+public enum BunDependencyType {
+    NORMAL,
+    DEV,
+    PEER,
+    OPTIONAL,
+    OPTIONAL_PEER
+}
